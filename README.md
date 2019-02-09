00a00c2d400246be3be9eafd979ab6a489969122
tkobialka <tkobialka@gmail.com>
2019-02-09 15:47:56 +1100

    Updated test

--- README.md
+++ README.md
@@ -12,4 +12,7 @@ Run:
 Runs the app in the development mode.<br>
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
+Test:
+> npm test
 
+Just a simple test added
--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../App';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "../rootReducer";
+import thunk from "redux-thunk";
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
